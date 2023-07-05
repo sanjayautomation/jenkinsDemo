4279263d562951ba3c6e3ca37dd42ffdc2b8aa88
sanjayautomation <treestechnospace@gmail.com>
2023-07-05 08:46:32 +0530

    Done1

--- src/test/java/com/TestLayer/basicfunction.java
+++ src/test/java/com/TestLayer/basicfunction.java
@@ -3,7 +3,7 @@ package com.TestLayer;
 public class basicfunction {
 
 	public static void main(String[] args) {
-		System.out.println("basic function");
+		System.out.println("basic functions");
 
 	}
 
